1fd6c440ca93c7eaa8571d8dd4293332d0c79498
Thanhdo1212 <thanhdo12122001@gmail.com>
2024-12-12 23:59:48 +0700

    react native state

--- App.tsx
+++ App.tsx
@@ -1,21 +1,21 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [count, setCount] = useState(0);
   //jsx
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
-      <View style={styles.parent}>
-        <Text style={styles.header}>thanhdo</Text>
-        <Text>
-          thanhdo
-          <Text style={styles.child}>bla bla</Text>
-        </Text>
-      </View>
-      <Text style={styles.hello1}>thanhdo</Text>
-      <Text>thanhdo</Text>
+      <Text style={{ fontSize: 30 }}>Count = {count}</Text>
+
+      <Button
+        onPress={() => {
+          setCount(count + 1);
+        }}
+        title="Increase"
+        color="red"
+      />
     </View>
   );
 }
@@ -28,27 +28,5 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "yellow",
-  },
-  hello1: {
-    color: "red",
-    fontSize: 60,
-    borderColor: "green",
-    borderWidth: 1,
-    padding: 10,
-  },
-  header: {
-    fontSize: 30,
-    fontWeight: "bold",
-  },
-  parent: {
-    fontSize: 60,
-    color: "green",
-    backgroundColor: "green",
-    // không kế thừa thuộc tính backgroud color của cha
-  },
-  child: {
-    fontSize: 30,
-    color: "pink",
   },
 });
